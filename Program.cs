d6ea27a967fbc7e1311245f6833f48515429b3db
frakhimov505 <87068169+frakhimov505@users.noreply.github.com>
2022-11-27 13:38:45 +0500

    upload

--- Program.cs
+++ Program.cs
@@ -50,13 +50,15 @@ Console.WriteLine($"Is your name {a}");
 Console.Write("Enter your age: ");
 string b = Console.ReadLine();
 int b_age  = Convert.ToInt32(b);
-Console.WriteLine($"Succesfully converted. Converted string was " + b_age);
+// Console.WriteLine($"Succesfully converted. Converted string was " + b_age);
+System.Console.WriteLine("Enter operation(+,-,*,/,%): ");
+string operation= System.Console.ReadLine();
 Console.Write("Enter his age: ");
 string hisAge = Console.ReadLine();
 int HisAge = Convert.ToInt32(hisAge);
-int ageDifference= b_age - HisAge;
-Console.WriteLine($"Difference between with your and his age is: " + (ageDifference));
-Console.WriteLine($"Is age difference bigger than HisAge: {ageDifference>=HisAge}");
+// int ageDifference= b_age - HisAge;
+// Console.WriteLine($"Difference between with your and his age is: " + (ageDifference));
+// Console.WriteLine($"Is age difference bigger than HisAge: {ageDifference>=HisAge}");
 System.Console.WriteLine("------------------------------------");
 
 // System.Console.WriteLine("===================");
@@ -74,23 +76,45 @@ System.Console.WriteLine("------------------------------------");
 // System.Console.WriteLine($"Is first number not equal to second: {ifirst!=isecond}");
 // System.Console.WriteLine("=========================");
 
-if (b_age==HisAge)
-{
-    System.Console.WriteLine($"Age of 1 person {b_age} is equal to age of second person {HisAge}");
-}
-else if(b_age>HisAge)
-{
-    System.Console.WriteLine($"Age of 1 person is bigger than age of second person!");
-}
+// if (b_age==HisAge)
+// {
+//     System.Console.WriteLine($"Age of 1 person {b_age} is equal to age of second person {HisAge}");
+// }
+// else if(b_age>HisAge)
+// {
+//     System.Console.WriteLine($"Age of 1 person is bigger than age of second person!");
+// }
 
-else
+// else
+// {
+//     System.Console.WriteLine($"Age of second person is bigger than age of first person!");
+// }
+
+// string message=
+//     b_age>HisAge
+//         ? "Age of 1 person is bigger than age of second person!"
+//         : "Age of second person is bigger than age of first person!";
+// System.Console.WriteLine(message);
+
+switch(operation)
 {
-    System.Console.WriteLine($"Age of second person is bigger than age of first person!");
+    case "+":
+        System.Console.WriteLine($"{b_age} + {HisAge} = {b_age+HisAge}");
+        break;
+
+    case "-":
+        System.Console.WriteLine($"{b_age} - {HisAge} = {b_age-HisAge}");
+        break;
+    case "*":
+        System.Console.WriteLine($"{b_age} * {HisAge} = {b_age*HisAge}");
+        break;
+    case "/":
+        System.Console.WriteLine($"{b_age} / {HisAge} = {b_age/HisAge}");
+        break;
+    case "%":
+        System.Console.WriteLine($"{b_age} % {HisAge} = {b_age%HisAge}");
+        break;
+        default:
+            System.Console.WriteLine("Operation not found!");
+        break;
 }
-
-string message=
-    b_age>HisAge
-        ? "Age of 1 person is bigger than age of second person!"
-        : "Age of second person is bigger than age of first person!";
-System.Console.WriteLine(message);
-
